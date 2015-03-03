5e4c2547b7d3cc7fb6f5a3ae204faf770b8392db
Ryan Spittler <ryanspittler@gmail.com>
2015-03-02 21:08:17 -0700

    Complete test one
    [#89457998]

--- koans/about_array_assignment.rb
+++ koans/about_array_assignment.rb
@@ -2,8 +2,8 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutArrayAssignment < Neo::Koan
   def test_non_parallel_assignment
-    names = ["John", "Smith", "III"]
-    assert_equal __, names
+    names = ["John", "Smith"]
+    assert_equal ["John", "Smith"], names
   end
 
   def test_parallel_assignments
